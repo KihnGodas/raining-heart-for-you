517de6c5c1d9a7c792169ebfba316ff52a6e323a
Khang <khanghihi06@gmail.com>
2024-08-01 21:22:38 +0700

    last commit for real

--- sketch.js
+++ sketch.js
@@ -5,9 +5,12 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   colors = ["#edbba8", "#e66f3c", "#c6b6d5", "#f1d147", "#a4cd98", "#95accb"];
 }
-
+function preload() {
+  img1 = loadImage("./image/photo_2024-08-01_21-02-55.jpg");
+}
 function draw() {
   background("#e5e1dc");
+  image(img1, 0, 0, windowWidth, windowHeight);
   for (let i = 0; i < hearts.length; i++) {
     hearts[i].display();
     hearts[i].fall();
